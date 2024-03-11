2d0a831429978f19da476005963db89664a65c42
James Escobar <escocodeseth@gmail.com>
2024-03-11 11:35:58 -0400

    inital commit

---
+++ variables.js
@@ -0,0 +1,13 @@
+// let admin ;
+// let name = 'john';
+
+// admin = name;
+
+// alert (admin);
+
+// let ourPlanet = 'Earth';
+
+// let currentUser = 'john';
+
+const birthday = '18.23.1999'
+const age = someCode(birthday)
